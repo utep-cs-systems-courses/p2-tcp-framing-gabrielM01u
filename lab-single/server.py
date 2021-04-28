895c5dece956efe9c24cd01cd300a97f1be6ec75
garielM01u <gmanzano@miners.utep.edu>
2021-04-28 00:39:48 -0600

    Initial additions: server

---
+++ lab-single/server.py
@@ -0,0 +1,27 @@
+import socket, sys, re, os
+from threading import Thread
+import threading
+
+threadCount = 0
+
+def worker_thread(conn, threadCount):
+    while True:
+        data = conn.recv(2048)
+        print(data.decode('utf-8'))
+        if not data:
+            break
+    conn.close()
+
+listenPort = 8000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', listenPort))
+s.listen(1) 
+
+while(True):
+    conn, addr = s.accept()
+    print('Connected to: ' + addr[0] + ':' + str(addr[1]))
+    threadCount + 1
+    x = threading.Thread(target=worker_thread, args=(conn, threadCount))
+    x.start()
+
