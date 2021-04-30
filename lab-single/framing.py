fec4b110dd1531625218246e0eba8b0fa8ddfc8c
garielM01u <gmanzano@miners.utep.edu>
2021-04-30 07:35:05 -0600

    added threeway handshake stuff

--- lab-single/framing.py
+++ lab-single/framing.py
@@ -3,7 +3,7 @@ import socket
 SYN_SEQ = b'\x00\x00\x00\x00'
 SYNACK_SEQ = b'\x00\x00\x00\x01'
 ACK_SEQ = b'\x00\x00\x00\x00'
-HEADER_LENGTH = 13
+HEADER_LENGTH = 18
 
 class frame:
 
@@ -14,8 +14,14 @@ class frame:
         self.port = port
         self.socket = socket
 
+# 1 = 0001 = SYN
+# 2 = 0010 = ACK
+# 3 = 0011 = SYNACK
+# 4 = 0100 = FIN
+# 9 = 1000 = RST
+
     def sendSYN(self, rport):
-        global ack
+
 
         src_port = self.port.to_bytes(2, 'big') # append src port
         header = bytearray(src_port)
@@ -24,6 +30,33 @@ class frame:
         header.append(SYN_SEQ)                  # append seq # for SYN packet
         header.append(self.ack.to_bytes(4,'big'))
         header.append(HEADER_LENGTH.to_bytes(1,'big')) ##13 bytes total
-        header.append(b'\x00\x00')
+        header.append(b'\x01') 
         self.socket.send(header)
-        pass
+
+
+    def synack_pkt(self, rport,ack):
+
+        src_port = self.port.to_bytes(2, 'big') # append src port
+        header = bytearray(src_port)
+        dest_port = rport.to_bytes(2,'big') 
+        header.append(dest_port)                # append dest port
+        header.append(SYNACK_SEQ)                  # append seq # for SYN packet
+        header.append(self.ack.to_bytes(4,'big'))
+        header.append(HEADER_LENGTH.to_bytes(1,'big')) ##18 bytes total
+        header.append(b'\x03')
+        header.append(ack.to_bytes(4,'big')) 
+        return header
+        
+
+    def ack_pkt(self, rport):
+        global ack
+
+        src_port = self.port.to_bytes(2, 'big') # append src port
+        header = bytearray(src_port)
+        dest_port = rport.to_bytes(2,'big') 
+        header.append(dest_port)                # append dest port
+        header.append(ACK_SEQ)                  # append seq # for SYN packet
+        header.append(self.ack.to_bytes(4,'big'))
+        header.append(HEADER_LENGTH.to_bytes(1,'big')) ##13 bytes total
+        header.append(b'\x02') 
+        return header
--- lab-single/server.py
+++ lab-single/server.py
@@ -1,17 +1,38 @@
 import socket, sys, re, os
+from framing import frame
 from threading import Thread
+
 
 
 threadCount = 0
 
-def worker_thread(conn, threadCount):
+def listener_thread(s,conn,addr, threadCount):
+    id = threadCount
+    rport = addr[1]
+    ack = 0
     while True:
         data = conn.recv(2048)
-        print(data.decode('utf-8'))
+        if tcp_logic(data) == 1:
+            ack = 1
+            s.send(packet.synack_packet(rport,ack))
+            ack = 0
+        elif tcp_logic(data) == 2:
+            s.send(packet.)
         if not data:
             break
     print("Disconnecting: " + addr[0])
     conn.close()
+
+def tcp_logic(data):
+
+    if data and len(data) == 14:
+        if data[:13] == b'\x01':
+            print('SYN recvd')
+            return 1
+        elif data[:13] == b'\x02':
+            print('ACK recvd')
+            return 2
+    
 
 listenPort = 8000
 
@@ -19,10 +40,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', listenPort))
 s.listen(1) 
 
+packet = frame(listenPort, s)
+
 while(True):
     conn, addr = s.accept()
     print('Connected to: ' + addr[0] + ':' + str(addr[1]))
     threadCount + 1
-    x = Thread(target=worker_thread, args=(conn, threadCount))
+    x = Thread(target=listener_thread, args=(s,conn,addr,threadCount))
     x.start()
 
